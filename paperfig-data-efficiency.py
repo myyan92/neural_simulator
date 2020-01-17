604fb83852892623c576ae74a6edb0079111cde2
mengyuan <mengyuan@bohg-ws-3.stanford.edu>
2020-01-17 15:11:13 -0800

    paper figures and scripts

---
+++ paperfig-data-efficiency.py
@@ -0,0 +1,28 @@
+import matplotlib
+import numpy as np
+font = {'family' : 'serif',
+        'weight' : 'normal',
+        'size'   : 16}
+matplotlib.rc('font', **font)
+import matplotlib.pyplot as plt
+
+DVF_data_size = [1.0, 0.1]
+DVF_avg_dists = [0.0072, 0.0082]
+DVF_max_dists = [0.0127, 0.0137]
+
+neural_data_size = [0.01, 0.033, 0.1, 0.25, 0.5, 1.0]
+neural_avg_dists = [0.0028, 0.0023, 0.0022, 0.0022, 0.0021, 0.0020]
+neural_max_dists = [0.0077, 0.0066, 0.0064, 0.0063, 0.0062, 0.0060]
+
+plt.semilogx(DVF_data_size, DVF_avg_dists, c='C1', label='DVF, average')
+plt.semilogx(DVF_data_size, DVF_max_dists, c='C1', linestyle='--', label='DVF, maximum')
+plt.semilogx(neural_data_size, neural_avg_dists, c='C0', label='ours, average')
+plt.semilogx(neural_data_size, neural_max_dists, c='C0', linestyle='--', label='ours, maximum')
+plt.ylim([0,0.02])
+plt.yticks(np.linspace(0,0.02,5))
+plt.xlabel('Relative dataset size')
+plt.ylabel(r'$\|S_{pred} - S_{gt}\| (m)$')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
--- paperfigs/paperfig.py
+++ paperfigs/paperfig.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib
-font = {'family' : 'normal',
+font = {'family' : 'serif',
         'weight' : 'normal',
-        'size'   : 16}
+        'size'   : 18}
 matplotlib.rc('font', **font)
 import matplotlib.pyplot as plt
 
@@ -11,8 +11,8 @@ neural_novision_train = np.load('eval_sim_trainset.npz')
 neural_vision_eval = np.load('eval_sim_testset_withvision.npz')
 neural_vision_train = np.load('eval_sim_trainset_withvision.npz')
 
-baseline_train = np.load('../baseline-DVF-0/eval_simseq3d_rollout_trainset.npz')
-baseline_eval =  np.load('../baseline-DVF-0/eval_simseq3d_rollout_testset.npz')
+baseline_train = np.load('../../baseline-DVF-0/eval_simseq3d_rollout_trainset.npz')
+baseline_eval =  np.load('../../baseline-DVF-0/eval_simseq3d_rollout_testset.npz')
 baseline_train_avg_dists=baseline_train['avg_dists']-baseline_train['start_avg_dists'].reshape((-1,1))
 baseline_train_max_dists=baseline_train['max_dists']-baseline_train['start_max_dists'].reshape((-1,1))
 baseline_train = {'mean_avg': np.mean(baseline_train_avg_dists, axis=0),
@@ -32,18 +32,18 @@ series = [(neural_vision_eval['mean_avg'], neural_vision_eval['std_avg']),
           (baseline_eval['mean_avg'], baseline_eval['std_avg']),
           (baseline_eval['mean_max'], baseline_eval['std_max']), ]
 
-labels = ['ours, average', 'ours, maximum', 'baseline, average', 'baseline, maximum']
+labels = ['ours, average', 'ours, maximum', 'DVF, average', 'DVF, maximum']
 colors = ['C0', 'C0', 'C1', 'C1']
 linestyles = ['-', '--', '-', '--']
 
 for data, label, c, l in zip(series, labels, colors, linestyles):
     mean = data[0][:50]
     std = data[1][:50]
-    plt.plot(mean, c=c, label=label, linestyle=l)
-#    plt.fill_between(np.arange(len(mean)), mean-std, mean+std, facecolor=c, alpha=0.2)
+    plt.plot(mean, c=c, label=label, linestyle=l, linewidth=2.5)
+    plt.fill_between(np.arange(len(mean)), mean-std, mean+std, facecolor=c, alpha=0.2)
 plt.axis([0, 50, 0, 0.25])
 plt.xlabel('Time step (t)')
-#plt.ylabel(r'$\|x_{rollout} - x_{gt}\|$')
+plt.ylabel(r'$\|S_{rollout} - S_{gt}\| (m)$')
 plt.legend(loc=2)
 plt.tight_layout()
 plt.show()
